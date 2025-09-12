22e4bbcd8e661a732407893a126851cba203d7f3
Marco Sihombing <71242666+marco-sihombing@users.noreply.github.com>
2025-09-12 16:36:36 +0700

    feat: adding additional security

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,7 +152,9 @@ export default function Home() {
 
       <>
         {overlay && (
-          <div className="fixed inset-0 bg-black z-[9999] opacity-90 flex items-center justify-center"></div>
+          <div className="fixed inset-0 bg-black z-[9999] opacity-90 flex items-center justify-center">
+          <p>Sorry, but please don't take screenshots</p>
+          </div>
         )}
       </>
 
